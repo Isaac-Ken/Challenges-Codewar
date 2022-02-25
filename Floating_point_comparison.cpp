35c62876a50945cb3349e40c8ccd73f325363d1c
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-02-24 20:55:14 -0600

    Update Floating_point_comparison.cpp

--- Floating_point_comparison.cpp
+++ Floating_point_comparison.cpp
@@ -1,6 +1,23 @@
+#Initial
 #include <cmath>
 using namespace std;
 
 bool approx_equals(double a, double b) {
   return round(a*100) == round(b*100);
 }
+
+#Simple sol
+#include <cmath>
+using namespace std;
+
+bool approx_equals(double a, double b) {
+  return abs(a - b) < 0.001;
+}
+
+#Complex sol
+#include <cmath>
+using namespace std;
+
+bool approx_equals (double a, double b, double rel_tol=1e-09, double abs_tol=0.001){
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol);
+  }
